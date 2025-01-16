2bbc363f5752e230422a298fea81cfb1b6fd8f07
danielace1 <danielsudharsan1805@gmail.com>
2025-01-16 14:23:19 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
-import helmet from "helmet";
 import connectDB from "./lib/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -13,7 +12,6 @@ import notificationRoutes from "./routes/notification.route.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
 app.use(
@@ -33,7 +31,15 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log("Server is running on port http://localhost:" + PORT);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect to the database.");
+  }
+};
+
+startServer();

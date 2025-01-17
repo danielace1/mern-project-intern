2647662d6da13eb40bb3c11bcc21ccac5de4556a
danielace1 <danielsudharsan1805@gmail.com>
2025-01-17 14:38:29 +0530

    fix: generateToken

--- server/lib/generateToken.js
+++ server/lib/generateToken.js
@@ -10,9 +10,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
     httpOnly: true,
     // secure: process.env.NODE_ENV !== "development",
     secure: true,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     // sameSite: "strict",
     sameSite: "none",
-    domain: ".vercel.app",
   });
 };

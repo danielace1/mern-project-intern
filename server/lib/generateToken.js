b8094f4203137552dca5c07e2356a869db6f8931
danielace1 <danielsudharsan1805@gmail.com>
2025-01-17 14:41:02 +0530

    fix: logout controller

--- server/lib/generateToken.js
+++ server/lib/generateToken.js
@@ -13,5 +13,6 @@ export const generateTokenAndSetCookie = (userId, res) => {
     // secure: process.env.NODE_ENV === "production",
     // sameSite: "strict",
     sameSite: "none",
+    domain: ".vercel.app",
   });
 };
